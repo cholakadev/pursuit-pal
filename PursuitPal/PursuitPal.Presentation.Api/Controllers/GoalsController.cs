13ee2e899227ea35d446eb23a0040c6c8e9f0d93
georgi.cholakov <georgi.cholakov@mentormate.com>
2024-01-30 15:19:31 +0200

    Endpoint routing and lowercase configuration

---
+++ PursuitPal/PursuitPal.Presentation.Api/Controllers/GoalsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PursuitPal.Presentation.Api.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public class GoalsController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateGoal([FromBody] object goal)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateGoal([FromBody] object goal)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetGoalById([FromQuery] Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllGoals()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteGoal([FromQuery] Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- PursuitPal/PursuitPal.Presentation.Api/Program.cs
+++ PursuitPal/PursuitPal.Presentation.Api/Program.cs
@@ -1,8 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
+builder.Services.AddApiVersioning(
+    options =>
+    {
+        options.ReportApiVersions = true;
+        options.DefaultApiVersion = new ApiVersion(1, 0);
+    });
+builder.Services.AddVersionedApiExplorer(
+    options =>
+    {
+        options.GroupNameFormat = "'v'VVV";
+        options.SubstituteApiVersionInUrl = true;
+    });
 
 var app = builder.Build();
 
