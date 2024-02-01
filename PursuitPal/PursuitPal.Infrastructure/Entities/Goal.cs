c5e5b56c918f252897844f9ef772f5bc9bbfb026
georgi.cholakov <georgi.cholakov@mentormate.com>
2024-02-01 13:38:53 +0200

    Fixed navigation proeprties

--- PursuitPal/PursuitPal.Infrastructure/Entities/Goal.cs
+++ PursuitPal/PursuitPal.Infrastructure/Entities/Goal.cs
@@ -1,15 +1,21 @@
-﻿namespace PursuitPal.Infrastructure.Entities
+﻿using PursuitPal.Core.Helpers;
+
+namespace PursuitPal.Infrastructure.Entities
 {
     public class Goal : Auditable
     {
         public Guid Id { get; set; }
 
+        public Guid UserId { get; set; }
+
         public DateTime FromDate { get; set; }
 
         public DateTime ToDate { get; set; }
 
-        public int Status { get; set; }
+        public GoalStatus Status { get; set; }
 
-        public Guid UserId { get; set; }
+        public GoalDetails Details { get; set; }
+
+        public IEnumerable<GoalFeedback> Feedbacks { get; set; }
     }
 }
--- PursuitPal/PursuitPal.Infrastructure/Entities/User.cs
+++ PursuitPal/PursuitPal.Infrastructure/Entities/User.cs
@@ -11,5 +11,7 @@
         public string Email { get; set; }
 
         public string Password { get; set; }
+
+        public IEnumerable<Goal> Goals { get; set; }
     }
 }
