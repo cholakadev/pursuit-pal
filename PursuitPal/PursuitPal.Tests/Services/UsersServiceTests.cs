cc5761a307c6477229b551a11a728247ec269348
georgi.cholakov <georgi.cholakov@mentormate.com>
2024-02-04 00:18:25 +0200

    Register user failed test

---
+++ PursuitPal/PursuitPal.Tests/Services/UsersServiceTests.cs
@@ -0,0 +1,52 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using NSubstitute;
+using PursuitPal.Core.Contracts.Repositories;
+using PursuitPal.Core.Exceptions.OperationExceptions;
+using PursuitPal.Core.Requests;
+using PursuitPal.Infrastructure.Entities;
+using PursuitPal.Services;
+
+namespace PursuitPal.Tests.Services
+{
+    public class UsersServiceTests
+    {
+        private readonly UsersService _sut;
+        private readonly IRepository<User> _usersRepository;
+        private readonly IConfiguration _configuration;
+
+        public UsersServiceTests()
+        {
+            _usersRepository = Substitute.For<IRepository<User>>();
+            _configuration = Substitute.For<IConfiguration>();
+
+            _sut = new UsersService(_usersRepository, _configuration);
+        }
+
+        [Fact]
+        public void Handle_WhenUserIsCreatedSuccessfully_ShouldReturnUserId()
+        {
+
+        }
+
+        [Fact]
+        public async Task Handle_WhenUserIsNotCreatedSuccessfully_ShouldThrowFailedCreationException()
+        {
+            _usersRepository
+                .AddAsync(Arg.Any<User>())
+                .Returns(default(User));
+
+            var act = async () => await _sut.RegisterUserAsync(Request);
+
+            await act.Should().ThrowAsync<FailedCreationException>();
+        }
+
+        private CreateUpdateUserRequest Request => new CreateUpdateUserRequest
+        {
+            Email = "Test",
+            FirstName = "Test",
+            LastName = "Test",
+            Password = "Test",
+        };
+    }
+}
